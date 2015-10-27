226eaa71a7850b70fb1c3593df884fafcd3c46bd
Conchylicultor <etiennefg.pot@gmail.com>
2015-10-27 09:13:05 +0100

    Update .pro and base code struct for messagemanager

--- ProjectDistributedAlgorithm.pro
+++ ProjectDistributedAlgorithm.pro
@@ -7,9 +7,13 @@ QMAKE_CXXFLAGS += -std=c++11
 
 SOURCES += main.cpp \
     nodeinfo.cpp \
-    udpwrapper.cpp
+    udpwrapper.cpp \
+    storemap.cpp \
+    messagemanager.cpp
 
 HEADERS += \
     nodeinfo.h \
-    udpwrapper.h
+    udpwrapper.h \
+    storemap.h \
+    messagemanager.h
 
--- messagemanager.h
+++ messagemanager.h
@@ -1,8 +1,22 @@
 #ifndef MESSAGEMANAGER_H
 #define MESSAGEMANAGER_H
 
+#include <list>
+
+struct Message
+{
+    // Destinataire
+    // Content (key, value)
+    // bool Ack
+    // VectorClock
+};
+
 class MessageManager
 {
+private:
+    std::list<Message> messagesToSend;
+    std::list<Message> messagesSent;
+
 public:
     MessageManager();
 };
